e1a32a610211dddfe54a5e650f4d8ef9746ad956
nishi0076 <nishitomo7396@gmail.com>
2023-07-09 08:03:00 +0900

    add new file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticatedController;
+use App\Http\Controllers\TimeStampController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,9 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('attendance.index');
-});
+Route::get('/', [TimeStampController::class, 'index'])
+->middleware('auth')
+->name('attendance.index');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
